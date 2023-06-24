9618713136a7dafc09a93957ff8e6a5baf87d3d9
quangh0409 <trongquangvu80@gmail.com>
2023-06-24 22:32:00 +0700

    fix: api

--- src/common/http.ts
+++ src/common/http.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: 'https://decision-help-system.onrender.com/api/v1/',
-//   headers: {
-//     'Content-type': 'application/json',
-//   },
-// });
-
 const api = axios.create({
-  baseURL: "http://127.0.0.1:6808/api/v1/",
+  baseURL: 'https://decision-help-system.onrender.com/api/v1/',
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
   },
 });
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:6808/api/v1/",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+// });
 
 export default api;
